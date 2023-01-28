cd9a67218c1336d2119a6a39fc90d6522cb13440
Olga Goncharenko <92627264+GoncharenkoO@users.noreply.github.com>
2023-01-28 12:33:07 +0200

    Add file

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import "./index.html";
+import "./index.scss";
+import { multiply, sum } from "./modules/calc";
+
+// const imgWrap = document.querySelector(".img");
+// const img = new Image();
+// img.src = minion;
+// img.width = 200;
+// imgWrap.append(img);
+
+console.log(multiply(3, 4));
+console.log(sum(3, 4));
+
